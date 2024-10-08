dae57e804295f5c4d463ce24afa4deb2b0fc6e1f
Davod <davodnikoyii@gmail.com>
2024-10-08 21:00:59 +0200

    Added React-navigation library and created a stack navigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, View, Text } from "react-native";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Home Screen</Text>
     </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Navigators/RootStackNavigator.tsx
@@ -0,0 +1,11 @@
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// const RootStack = createNativeStackNavigator();
+
+// export function RootStackNavigator() {
+//     return{
+//         <RootStack.Navigator >
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//       </RootStack.Navigator>
+//     }
+// }
